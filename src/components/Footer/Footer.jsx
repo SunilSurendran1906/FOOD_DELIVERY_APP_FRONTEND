d7ded8a1d8a361a80d2ed6e7a53d58d4e99888b7
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2024-06-20 13:19:34 +0530

    FRONTEND FOODAPP

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Experience the best in food delivery with FoodDeliveryApp. From
+            local favorites to gourmet delights, we bring delicious meals right
+            to your doorstep. Our mission is to make your dining experience
+            convenient and enjoyable, with fast delivery, secure payment
+            options, and exceptional customer service. Whether you're at home or
+            at the office, we've got your cravings covered.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
